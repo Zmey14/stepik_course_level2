21fd291ffe611f7888034748f165c48568f42443
Zmey14 <mishkaneshishka@gmail.com>
2022-04-04 11:11:45 +0300

    home work set github_update_1

--- lesson3_1_step1.py
+++ lesson3_1_step1.py
@@ -2,5 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-browser = webdriver.Chrome()
-browser.get()
+link = "https://stepik.org/lesson/187065/step/11?unit=161976"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+finally:
+    browser.quit()
