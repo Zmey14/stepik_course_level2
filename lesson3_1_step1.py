2a306ed1f5d6428e1a946abc64ed6a29e4f74cbb
Zmey14 <mishkaneshishka@gmail.com>
2022-04-05 17:59:33 +0300

    start learning pytest

--- lesson3_1_step1.py
+++ lesson3_1_step1.py
@@ -1,12 +1,24 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 
-link = "https://stepik.org/lesson/187065/step/11?unit=161976"
+# def test_input_text(expected_result, actual_result):
+#     assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
 
-try:
-    browser = webdriver.Chrome()
-    browser.get(link)
+# print(test_input_text(8,11))
+# print(test_input_text(11,11))
+# print(test_input_text(11,15))
 
-finally:
-    browser.quit()
+# s = "My name is Julia"
+#
+# if "name" in s:
+#     print("Substring found")
+#
+# index = s.find("name")
+# if index != -1:
+#     print(f'substring found at index {index}')
+
+# def test_substring(full_string, substring):
+#     assert substring in full_string, f" expected '{substring}' to be substring of '{full_string}'"
+
+# print(test_substring("fulltext", "some_value"))
+# print(test_substring("1", "1"))
+# print(test_substring("some_text", "some"))
